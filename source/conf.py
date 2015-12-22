3871f4ae59778282237c815480614e7e5e4248dd
Niek Bosch <code@niekbosch.nl>
2015-12-22 11:14:59 +0100

    Updated conf.py to make the Sphinx extension work within the readthedocs.com build environment

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../extensions/youtube/lib'))
+sys.path.insert(0, os.path.normpath( os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), '../extensions/youtube/lib/' ) ) )
 
 # -- General configuration ------------------------------------------------
 
